9e6e6d4aa6c7ca15bb5e514517218bef44a380ba
Naoki_Desktop <naokigsyoshida@gmail.com>
2022-02-24 17:27:35 +0900

    置ける場所の探索

--- othello.cpp
+++ othello.cpp
@@ -6,3 +6,14 @@ GameOthello::GameOthello(){
     board[4][3]=WHITE;
     board[4][4]=BLACK;
 }
+
+void GameOthello::updateAble(){
+
+}
+
+int GameOthello::searchAble(std::pair<int,int> pos, std::pair<int,int> vec){
+    if((pos.first<0 || 8<=pos.first) || (pos.second<0 || 8<=pos.second)){
+        return -1;
+    }
+    
+}
--- othello.h
+++ othello.h
@@ -6,13 +6,16 @@
 #define WHITE 2
 
 class GameOthello{
+    private:
+    int searchAble(std::pair<int,int>, std::pair<int,int>);
     public:
-    int board[8][8]={};
-    int turn=BLACK;
+    int board[8][8]={}; //現在の盤面
+    int turn=BLACK; //現在のターン
+    int able[8][8]={}; //石を置ける場所と返せる数
     
 
     GameOthello();
-
+    void updateAble(void);
 
 
 
